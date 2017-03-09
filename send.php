d24d6b4304455a82237ff67b5ffdea80010f9b3e
Rohith <rohith.mudaliar@gmail.com>
2017-03-09 11:50:45 +0530

    almost done

--- send.php
+++ send.php
@@ -8,12 +8,13 @@
 </head>
 <body>
 <div class="heading">
+<img src="logo.png">
 	<h1>Kilobyte File Sharing</h1>
 </div>
 <form action="upload.php" method="post" enctype="multipart/form-data" id="myForm">
 
     <input type="file" name="fileToUpload" id="fileToUpload" class="display-none">
-    <input type="submit" value="Upload Image" name="submit">
+    <input type="submit" value="Send File" name="submit">
 </form>
 <div class="pbar">
 	<canvas width="256" height="256"></canvas>
